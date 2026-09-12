ce6600738826bd90e7b9696742225671958726eb
Utkarsh07kumar <126189600+Utkarsh07kumar@users.noreply.github.com>
2026-09-13 01:14:20 +0530

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res= new ArrayList<> ();
+        List <Integer> ans = new ArrayList<> ();
+        f(0,nums,ans,res);
+                // convert res into a Set to remove duplicates
+        Set<List<Integer>> set = new HashSet<>(res);
+
+        // convert back into a List to return
+        List<List<Integer>> finalRes = new ArrayList<>(set);
+
+        return finalRes;
+    }
+    private void f (int i,int[] nums, List<Integer> ans,List<List<Integer>> res){
+        if(i>=nums.length){
+            res.add(new ArrayList<>(ans));
+            return;
+        }
+        ans.add(nums[i]);
+        f(i+1,nums,ans,res);
+        ans.remove(ans.size() - 1);
+        f(i+1,nums,ans,res);
+    }
+}
+
